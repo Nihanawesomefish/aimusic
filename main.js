745728fd3101e97563190d282ea2ff174de9de91
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2022-12-03 11:14:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(650,400);
+canvas.center();
+console.log("cocolachocolate");
+}
+function draw(){
+background("black");
+console.log("cococolaesestoma");
+}
