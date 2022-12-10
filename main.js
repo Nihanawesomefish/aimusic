6286e1c22f01f2a09e2a5ac39d75401412242d0f
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2022-12-10 12:27:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 function preload(){
-
+song1=loadSound("music.mp3");
+song2=loadSound("music2.mp3");
 }
 function setup(){
-canvas=createCanvas(650,400);
+canvas=createCanvas(700,450);
 canvas.center();
+video=createCapture(VIDEO);
+video.hide();
 console.log("cocolachocolate");
+poseNet=ml5.poseNet(video,modelLoaded);
+//poseNet.on('pose',gotPoses)
 }
 function draw(){
 background("black");
 console.log("cococolaesestoma");
+image(video, 0,0,700,450);
 }
+function modelLoaded(){
+console.log("posenet is initialized")
+}
+function gotPoses(results){
+    if(results.length>0){
+     console.log(results);
+     leftwristx=results[0].pose.leftWrist.x;
+     leftwristy=results[0].pose.leftWrist.y;
+     rightwristx=results[0].pose.rightWrist.x;
+     rightwristy=results[0].pose.rightWrist.y;
+     score_leftwrist=results[0].pose.keypoints[9].score;
+     score_rightwrist=results[0].pose.keypoints[10].score;
+    }
+    }
